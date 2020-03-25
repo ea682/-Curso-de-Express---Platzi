8b30fcd2faf4840792b49c122e71d32e713a78ad
ea682 <erik.ahumada@lag.cl>
2020-03-25 00:55:05 -0300

    Se agrega ejemplo de rutas y un template-engine

--- express-app-gen/app.js
+++ express-app-gen/app.js
@@ -21,6 +21,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/test', function(req, res, next){
+  res.send({
+    'name': 'Erik'
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
